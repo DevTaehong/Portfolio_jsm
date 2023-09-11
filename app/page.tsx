f18731a5b294005a4e84f2ba0190093054051e0b
DevTaehong <minth1123@icloud.com>
2023-09-11 18:08:18 -0300

    feat: add three sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import FeaturedProjects from "@/components/FeaturedProjects";
 import Hero from "@/components/Hero";
 import MySkills from "@/components/MySkills";
 import ServiceIProvide from "@/components/ServiceIProvide";
+import Testimonials from "@/components/Testimonials";
+import WorkExperience from "@/components/WorkExperience";
 
 export default function Home() {
   return (
-    <main className="bg-white800 dark:bg-black300">
+    <main>
       <Hero />
       <MySkills />
       <ServiceIProvide />
+      <WorkExperience />
+      <FeaturedProjects />
+      <Testimonials />
     </main>
   );
 }
