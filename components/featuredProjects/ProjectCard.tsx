0094765f2e8409c57bb3bb8456525b664b9cda6d
DevTaehong <minth1123@icloud.com>
2023-09-23 17:19:37 -0300

    feat: add an animation in the ProjectCard component

--- components/featuredProjects/ProjectCard.tsx
+++ components/featuredProjects/ProjectCard.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
 import { Card } from "@nextui-org/card";
 import { Link } from "@nextui-org/link";
 import Image from "next/image";
@@ -24,76 +28,93 @@ const ProjectCard = ({
     hipnode: "bg-hipnode",
   };
 
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
   return (
-    <Card
-      className={`${
-        colorVariants[project.name.toLowerCase()]
-      } flex flex-col justify-center pb-[2.99rem] pt-[2.88rem] 
-                xl:pb-[5.81rem] xl:pt-[6.06rem] ${
-                  (index + 1) % 2 === 0 ? "xl:pr-[4.38rem]" : "xl:pl-[6.75rem]"
-                }`}
+    <motion.div
+      style={{
+        scale: scrollYProgress,
+        opacity: scrollYProgress,
+      }}
+      ref={ref}
     >
-      <div
-        className={`flex flex-col gap-6  ${
-          (index + 1) % 2 === 0 ? "md:flex-row-reverse" : "md:flex-row"
-        }`}
+      <Card
+        className={`${
+          colorVariants[project.name.toLowerCase()]
+        } flex flex-col justify-center pb-[2.99rem] pt-[2.88rem] 
+                  xl:pb-[5.81rem] xl:pt-[6.06rem] ${
+                    (index + 1) % 2 === 0
+                      ? "xl:pr-[4.38rem]"
+                      : "xl:pl-[6.75rem]"
+                  }`}
       >
         <div
-          className={`flex flex-col gap-[1.88rem] xl:gap-[3.13rem] ${
-            (index + 1) % 2 === 0 && "sm:pl-0 sm:pr-6 xl:pr-0"
-          } pl-6 xl:pl-0`}
+          className={`flex flex-col gap-6  ${
+            (index + 1) % 2 === 0 ? "md:flex-row-reverse" : "md:flex-row"
+          }`}
         >
-          <div className="flex flex-col gap-[1.88rem] xl:gap-[1.44rem]">
-            <h2 className="projectName xl:max-w-[31.1875rem] xl:text-[3rem] xl:tracking-[-0.03rem]">
-              {project.name} {" - "} <br className="block xl:hidden" />
-              {project.description}
-            </h2>
-            <div className="flex flex-row gap-1 truncate xl:gap-5">
-              {project.techStack.map((tech) => (
-                <div
-                  key={tech}
-                  className="smallRegular techStack flex items-start text-white900"
-                >
-                  {tech}
-                </div>
-              ))}
-            </div>
-          </div>
-          <Link
-            href={`/case-studies/${project.name}`}
-            className="bodyBold inline-flex gap-[0.47rem] text-white900"
+          <div
+            className={`flex flex-col gap-[1.88rem] xl:gap-[3.13rem] ${
+              (index + 1) % 2 === 0 && "sm:pl-0 sm:pr-6 xl:pr-0"
+            } pl-6 xl:pl-0`}
           >
-            See Detail Project
+            <div className="flex flex-col gap-[1.88rem] xl:gap-[1.44rem]">
+              <h2 className="projectName xl:max-w-[31.1875rem] xl:text-[3rem] xl:tracking-[-0.03rem]">
+                {project.name} {" - "} <br className="block xl:hidden" />
+                {project.description}
+              </h2>
+              <div className="flex flex-row gap-1 truncate xl:gap-5">
+                {project.techStack.map((tech) => (
+                  <div
+                    key={tech}
+                    className="smallRegular techStack flex items-start text-white900"
+                  >
+                    {tech}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <Link
+              href={`/case-studies/${project.name}`}
+              className="bodyBold inline-flex gap-[0.47rem] text-white900"
+            >
+              See Detail Project
+              <Image
+                src={arrow}
+                alt="See Detail Project Arrow"
+                width={35}
+                height={0}
+              />
+            </Link>
+          </div>
+          <div
+            className={`flex justify-center xl:absolute xl:inset-0 xl:mb-[3.31rem] xl:mt-[3.87rem] xl:overflow-hidden
+                      ${
+                        (index + 1) % 2 === 0
+                          ? "xl:mr-[40.32rem]"
+                          : "xl:ml-[37.27rem]"
+                      }`}
+          >
             <Image
-              src={arrow}
-              alt="See Detail Project Arrow"
-              width={35}
-              height={0}
+              className={`sm:h-full sm:w-full xl:h-[20.56731rem] ${
+                (index + 1) % 2 === 0
+                  ? "xl:translate-x-[-3.72rem]"
+                  : "xl:translate-x-[1.17rem]"
+              } `}
+              src={project.image}
+              width={320}
+              height={155}
+              alt={`Project ${project.name}`}
             />
-          </Link>
+          </div>
         </div>
-        <div
-          className={`flex justify-center xl:absolute xl:inset-0 xl:mb-[3.31rem] xl:mt-[3.87rem] xl:overflow-hidden
-                    ${
-                      (index + 1) % 2 === 0
-                        ? "xl:mr-[40.32rem]"
-                        : "xl:ml-[37.27rem]"
-                    }`}
-        >
-          <Image
-            className={`sm:h-full sm:w-full xl:h-[20.56731rem] ${
-              (index + 1) % 2 === 0
-                ? "xl:translate-x-[-3.72rem]"
-                : "xl:translate-x-[1.17rem]"
-            } `}
-            src={project.image}
-            width={320}
-            height={155}
-            alt={`Project ${project.name}`}
-          />
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </motion.div>
   );
 };
 
